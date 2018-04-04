d59a4d7ec12b77250e2d6b150690f69c2996c800
Anna Marie Cooper and Stephanie Faber <stephanie@>
2018-04-04 11:52:13 -0700

    add authentication service and add login to app component.

--- src/app/api-keys.ts
+++ src/app/api-keys.ts
@@ -1,8 +1,8 @@
 export var masterFirebaseConfig = {
-  apiKey: "AIzaSyDNVkvO4dfEyZINZVTwUdZ0Eawvw3ZUpvE",
-  authDomain: "mystore-5d5d2.firebaseapp.com",
-  databaseURL: "https://mystore-5d5d2.firebaseio.com",
-  projectId: "mystore-5d5d2",
-  storageBucket: "mystore-5d5d2.appspot.com",
-  messagingSenderId: "233300515035"
+  apiKey: "AIzaSyDL3nvCofZDIVLmO2JgjGbLNfxM2SN7wYw",
+  authDomain: "crowdfunder-cd694.firebaseapp.com",
+  databaseURL: "https://crowdfunder-cd694.firebaseio.com",
+  projectId: "crowdfunder-cd694",
+  storageBucket: "",
+  messagingSenderId: "236216536372"
 };
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,4 @@
 export class User {
-  public userImage: string = 
+  public userImage: string = "https://art.aminoapps.com/static/img/user-icon-placeholder.png";
   constructor(public userName: string, public email: string){}
 }
